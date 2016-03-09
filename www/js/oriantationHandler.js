c3c42aa6a60541bfefd2a839a687b9ad37756808
Avi Yaakov <aviyaakov1@gmail.com>
2016-03-09 10:42:01 +0200

    bug fix

--- www/js/oriantationHandler.js
+++ www/js/oriantationHandler.js
@@ -1,21 +1,9 @@
 (function () {
     'use strict';
     var image = document.getElementById('camImage');
-    if (screen.orientation.type === "portrait-primary") {
-        image.src = "file:///android_asset/www/img/portrait.jpg";
-        image.className = "portrait";
-    } else {
-        image.src = "file:///android_asset/www/img/landscape.jpg";
-        image.className = "landscape";
-    }
+
     window.addEventListener("orientationchange", function () {
-        if (image.src === "file:///android_asset/www/img/portrait.jpg") { // example image
-            image.src = "file:///android_asset/www/img/landscape.jpg";
-        } else if (image.src === "file:///android_asset/www/img/landscape.jpg") {
-            image.src = "file:///android_asset/www/img/portrait.jpg";
-        }
-        
-        if (screen.orientation.type === "portrait-primary") {
+        if (screen.orientation.type === "portrait-primary" && image.height < image.width) {
             image.className = "portrait";
         } else {
             image.className = "landscape";
--- www/js/touchHandler.js
+++ www/js/touchHandler.js
@@ -1,6 +1,13 @@
 var touchInit = (function () {
     'use strict';
-    var image = document.getElementById('camImage');
+    var image = document.getElementById('camImage'),
+        distanceArr = [],
+        viewWidth = image.width,
+        imageSize = image.width,
+        maxZoom = image.width * 2,
+        minZoom = image.width / 2,
+        marginLeft = 0;
+    
     function touchHandler(colorName, changeColor) {
         image.addEventListener('touchend', function (e) {
             var pColor,
@@ -10,7 +17,7 @@ var touchInit = (function () {
                 yCord = parseInt(touchobj.clientY, 10), // get y position of touch point relative to top edge (decimal)
                 canvas = document.createElement('canvas'),
                 ctx = canvas.getContext('2d');
-            canvas.width = image.width;
+            canvas.width = image.width + imageBounds.left;
             canvas.height = image.height + imageBounds.top; // for when the image is not overflowing 
 
             ctx.drawImage(image, imageBounds.left, imageBounds.top, image.width, image.height);
